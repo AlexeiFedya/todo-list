594334f5557fb6b970e14dec10903df533901189
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 17:37:25 +0300

    Updates

---
+++ precache-manifest.aa9c95e418534547c19861b31608771f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699ac09f4d2de8c115432fa179193b7e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8f369a20d0410ff9a266",
+    "url": "/todo-list/static/css/main.61cdac70.chunk.css"
+  },
+  {
+    "revision": "4c8465979e1bec2cd691",
+    "url": "/todo-list/static/js/2.b5d1a3b2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-list/static/js/2.b5d1a3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f369a20d0410ff9a266",
+    "url": "/todo-list/static/js/main.01b21686.chunk.js"
+  },
+  {
+    "revision": "e35d169da06fded0b1d1",
+    "url": "/todo-list/static/js/runtime-main.7b98a2f6.js"
+  }
+]);
